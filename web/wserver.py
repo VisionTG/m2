7bc0d6e4cb0df237d3615d1ff08e5baea0090c5f
Vision <105115761+VisionTG@users.noreply.github.com>
2022-08-24 19:09:16 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -227,13 +227,13 @@ function s_validate() {
           src="https://telegra.ph/file/43af672249c94053356c7.jpg"
           alt="logo"
         />
-        <a href="https://t.me/anas_tayyar">
+        <a href="https://t.me/TGFilmZone">
           <h2 class="name">Bittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        <a href="https://www.github.com/RangoTG/RangoTG "><i class="fab fa-github"></i></a>
+        <a href="https://t.me/TGFilmZone"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <div id="sticks">
@@ -619,13 +619,13 @@ section span{
           src="https://telegra.ph/file/43af672249c94053356c7.jpg"
           alt="logo"
         />
-        <a href="https://t.me/anas_tayyar">
+        <a href="https://t.me/TGFilmZone">
           <h2 class="name">Bittorrent Selection</h2>
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        <a href="https://www.github.com/RangoTG/m2"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/TGFilmZone"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -774,7 +774,7 @@ def set_priority(id_):
 
 @app.route('/')
 def homepage():
-    return "<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>"
+    return "<h1>See Cloudx Bot 2 Running Successfully</h1>"
 
 @app.errorhandler(Exception)
 def page_not_found(e):
